8a2ec4f9f7e8ea5cdb3fa40fd497dbbf904142d8
calf0530 <calf0530@naver.com>
2019-08-19 09:53:12 +0900

    곱하기 메소드 추가

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -12,4 +12,8 @@ public class Calculator {
     public double divide(double a, double b) {
         return a / b;
     }
+
+    public int subtract(int a, int b) {return a - b; }
+
+    public int mutiply(int a, int b) {return a * b; }
 }
