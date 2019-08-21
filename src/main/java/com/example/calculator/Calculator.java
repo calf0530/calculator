4a2e9983f34d73e671e925795ac7a57bd865f1ac
Seungwoo Jo <whtmddn940@gamil.com>
2019-08-21 15:07:10 +0900

    upload newet files

---
+++ src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.example.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public int subtract(int a, int b) {return a - b; }
+
+    public int mutiply(int a, int b) {return a * b; }
+}
